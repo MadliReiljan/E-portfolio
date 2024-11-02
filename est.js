f97c7836d1445c498a2ead2716402e1ecbc4f131
Madli Reiljan <madlireiljan@gmail.com>
2024-11-02 14:56:52 +0200

    New pages and pictures

---
+++ est.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const textElement = document.getElementById('typed-text');
+    const textToType = "Noorem Tarkvaraarendaja.";
+  
+    function typeWriterEffect(text, index) {
+      if (index < text.length) {
+        textElement.innerHTML += text.charAt(index);
+        index++;
+        setTimeout(function () {
+          typeWriterEffect(text, index);
+        }, 90);
+      }
+    }
+  
+    typeWriterEffect(textToType, 0);
+  });
+  
