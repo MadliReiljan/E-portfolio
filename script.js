5d1b3feae984ee70b24d9bb7aeda6c103b183fed
MadliReiljan <madlireiljan@gmail.com>
2024-11-02 21:05:31 +0200

    Filled the pages and added pictures

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const images = [
-    'assets/portfolio_picture.png',
-    'assets/profile.jpg',
-    'assets/portfoolio_pilt.jpg'
+    'assets/plants.png',
+    'assets/medrawing.jpg',
+    'assets/oceansaway.jpg'
 ];
 
 let currentIndex = 0;
 const rotatingImage = document.getElementById('rotating-image');
 
-// Set the initial image source
 rotatingImage.src = images[currentIndex];
+rotatingImage.style.transition = 'opacity 0.8s'; 
+rotatingImage.style.width = '90%';
+rotatingImage.style.height = 'auto';
 
 function updateImage() {
     rotatingImage.style.opacity = 0; // Fade out
@@ -16,11 +18,9 @@ function updateImage() {
         currentIndex = (currentIndex + 1) % images.length; // Increment index
         rotatingImage.src = images[currentIndex]; // Change image source
         rotatingImage.style.opacity = 1; // Fade in
-    }, 800); // Wait for fade out to finish
+    }, 800); 
 }
 
-// Set initial delay for starting the rotation
 setTimeout(() => {
-    // Set interval for automatic image rotation
-    setInterval(updateImage, 5000); // Change image every 3 seconds
-}, 800); // Wait half a second before starting the interval
+    setInterval(updateImage, 5000); 
+}, 800);
